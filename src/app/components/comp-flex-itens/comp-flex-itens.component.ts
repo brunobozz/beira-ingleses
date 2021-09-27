11e7f97aaa22f9aea9b63dda3d85381afcaafaf7
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2021-09-27 16:33:29 -0300

    flex itens

--- src/app/components/comp-flex-itens/comp-flex-itens.component.ts
+++ src/app/components/comp-flex-itens/comp-flex-itens.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comp-flex-itens',
   templateUrl: './comp-flex-itens.component.html',
-  styleUrls: ['./comp-flex-itens.component.scss']
+  styleUrls: ['./comp-flex-itens.component.scss'],
 })
 export class CompFlexItensComponent implements OnInit {
+  itens = [
+    { icon: 'fa-map-marker', text: 'Pé na Areia' },
+    { icon: 'fa-wifi', text: 'Wi-Fi' },
+    { icon: 'fa-tv', text: 'Smart TV' },
+    { icon: 'fa-snowflake-o', text: 'Ar Condicionado' },
+    { icon: 'fa-car', text: 'Garagem' },
+    { icon: 'fa-building', text: 'Elevador' },
+    { icon: 'fa-users', text: 'Até 6 Pessoas' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
