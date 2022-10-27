8aa48039745c873af0092a5598a20061775c3867
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-10-27 16:56:46 -0300

    new dates

---
+++ src/app/pages/page-home/content.ts
@@ -0,0 +1,286 @@
+export const itemsApto: any = [
+  {
+    img: 'wifi',
+    text: 'Internet Wifi de 250mb',
+  },
+  {
+    img: 'tv',
+    text: 'Smart TV 65 polegadas',
+  },
+  {
+    img: 'cozinha',
+    text: 'Cozinha',
+  },
+  {
+    img: 'vista_mar',
+    text: 'Vista para o mar',
+  },
+  {
+    img: 'cama_solteiro',
+    text: '2 Quartos',
+  },
+  {
+    img: 'sacada',
+    text: 'Sacada',
+  },
+  {
+    img: 'churrasco',
+    text: 'Churrasqueira',
+  },
+  {
+    img: 'user_plus',
+    text: 'Até 6 pessoas',
+  },
+];
+
+export const meses: any = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Augosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+];
+
+export const fotosPraia: any = [
+  'praia/01',
+  'praia/02',
+  'praia/03',
+  'praia/04',
+  'praia/05',
+  'praia/06',
+  'praia/07',
+  'praia/08',
+  'praia/09',
+  'praia/10',
+  'praia/11',
+  'praia/12',
+];
+
+export const week: any = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
+
+export const datas: any = [
+  // ============================================ NOVEMBRO
+  {
+    date: new Date(2022, 10, 12),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2022, 10, 13),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2022, 10, 14),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+
+  // ============================================ DEZEMBRO
+  {
+    date: new Date(2022, 11, 26),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2022, 11, 27),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2022, 11, 28),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+
+  // ============================================ JANEIRO
+  {
+    date: new Date(2023, 0, 2),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 3),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 4),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 5),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 6),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 7),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 8),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 9),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 10),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 11),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 12),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 13),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 16),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 17),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 18),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 19),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 20),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 21),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 22),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 23),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 24),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 25),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 26),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 27),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 28),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 29),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 30),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 0, 31),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+
+  // ============================================ FEVEREIRO
+  {
+    date: new Date(2023, 1, 1),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 1, 2),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 1, 3),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 1, 4),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 1, 5),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 1, 6),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 1, 7),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+  {
+    date: new Date(2023, 1, 8),
+    bg: 'rgba(163, 0, 217,0.5)',
+    color: '#ffffff',
+  },
+];
--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -1,77 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { datas, fotosPraia, itemsApto, meses, week } from './content';
 
 @Component({
   selector: 'app-page-home',
   templateUrl: './page-home.component.html',
   styleUrls: ['./page-home.component.scss'],
 })
-export class PageHomeComponent implements OnInit {
-  public itemsApto: any = [
-    {
-      img: 'wifi',
-      text: 'Internet Wifi de 250mb',
-    },
-    {
-      img: 'tv',
-      text: 'Smart TV 65 polegadas',
-    },
-    {
-      img: 'cozinha',
-      text: 'Cozinha',
-    },
-    {
-      img: 'vista_mar',
-      text: 'Vista para o mar',
-    },
-    {
-      img: 'cama_solteiro',
-      text: '2 Quartos',
-    },
-    {
-      img: 'sacada',
-      text: 'Sacada',
-    },
-    {
-      img: 'churrasco',
-      text: 'Churrasqueira',
-    },
-    {
-      img: 'user_plus',
-      text: 'Até 6 pessoas',
-    },
-  ];
-
-  public fotosPraia: any = [
-    'praia/01',
-    'praia/02',
-    'praia/03',
-    'praia/04',
-    'praia/05',
-    'praia/06',
-    'praia/07',
-    'praia/08',
-    'praia/09',
-    'praia/10',
-    'praia/11',
-    'praia/12',
-  ];
-
-  public months: any = [
-    'Janeiro',
-    'Fevereiro',
-    'Março',
-    'Abril',
-    'Maio',
-    'Junho',
-    'Julho',
-    'Augosto',
-    'Setembro',
-    'Outubro',
-    'Novembro',
-    'Dezembro',
-  ];
-
-  public week = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
+export class PageHomeComponent {
+  public itemsApto: any = itemsApto;
+  public fotosPraia: any = fotosPraia;
+  public months: any = meses;
+  public week = week;
 
   public colors: any = {
     daysMonthBgColor: 'rgba(254, 252, 139,0.5)',
@@ -82,25 +21,5 @@ export class PageHomeComponent implements OnInit {
     todayBdColor: 'rgb(0, 0, 0)',
   };
 
-  public dates: any = [
-    {
-      date: new Date(2022, 11, 12),
-      bg: 'rgba(163, 0, 217,0.5)',
-      color: '#ffffff',
-    },
-    {
-      date: new Date(2022, 11, 13),
-      bg: 'rgba(163, 0, 217,0.5)',
-      color: '#ffffff',
-    },
-    {
-      date: new Date(2022, 11, 14),
-      bg: 'rgba(163, 0, 217,0.5)',
-      color: '#ffffff',
-    },
-  ];
-
-  constructor() {}
-
-  ngOnInit(): void {}
+  public dates: any = datas;
 }
